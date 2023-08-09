7ce87a2869686d689f81cecd0cef7f14bd3e4e63
Proudmuslim <proudmuslim-dev@protonmail.com>
2023-08-09 13:13:09 -0700

    wip: Create basic schema
    Massive delay on this project due to IRL issues; still need to decide on webserver, will most likely use Poem or Rocket

---
+++ migrations/20230809195830_excerpts.sql
@@ -0,0 +1,13 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS excerpt
+(
+    id      INTEGER PRIMARY KEY NOT NULL,
+    excerpt STRING              NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS image
+(
+    path    STRING PRIMARY KEY NOT NULL,
+    post_id INTEGER NOT NULL,
+    FOREIGN KEY(post_id) REFERENCES excerpt(id)
+);
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use color_eyre::eyre::Result;
+use sqlx::SqlitePool;
+use std::env;
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+    let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
+
+    Ok(())
 }
