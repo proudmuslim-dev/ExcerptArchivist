47846493e4a06265b11a03a6730600a7879a7a70
Proudmuslim <proudmuslim-dev@protonmail.com>
2023-08-10 16:53:27 -0700

    wip: Migrate to Prisma due to issues with sqlx
    Webserver will most likely be Rocket

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,7 @@
 use color_eyre::eyre::Result;
-use sqlx::SqlitePool;
-use std::env;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
-    let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
-
     Ok(())
 }
