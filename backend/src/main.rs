64ad13297532016169ae7c0944212f11a1b96bd9
Proudmuslim <proudmuslim-dev@protonmail.com>
2023-08-10 20:12:35 -0700

    fix(ci): Fix Clippy workflow + add settle on Rocket as backend webserver

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,7 +1,17 @@
+#[macro_use]
+extern crate rocket;
+
 use color_eyre::eyre::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     color_eyre::install()?;
+
+    rocket::build()
+        .mount("/", routes![])
+        .launch()
+        .await?;
+
     Ok(())
-}
+} 
+
