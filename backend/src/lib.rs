1c80ce2e20a14e1d9819ddf6b261913cc9b38359
Proudmuslim <proudmuslim-dev@protonmail.com>
2023-08-14 10:48:17 -0700

    wip: Begin implementing routes for browsing

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,2 +1,7 @@
+#[macro_use]
+extern crate rocket;
+
+// TODO: Decide whether to have an auth system or not
+
 pub mod models;
 pub mod routes;
--- backend/src/models/prisma_ext/excerpt.rs
+++ backend/src/models/prisma_ext/excerpt.rs
@@ -4,7 +4,7 @@ use crate::models::{excerpts, prisma::excerpt};
 
 excerpt::include!(excerpt_with_images { images });
 
-pub async fn get_excerpt(excerpt_id: i32) -> Result<Option<excerpt_with_images::Data>, QueryError> {
+pub async fn find_excerpt(excerpt_id: i32) -> Result<Option<excerpt_with_images::Data>, QueryError> {
     excerpts()
         .await
         .find_unique(excerpt::id::equals(excerpt_id))
