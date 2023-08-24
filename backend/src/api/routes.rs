6f98bea734382d48850db38653eff2ee4f27e221
Proudmuslim <proudmuslim-dev@protonmail.com>
2023-08-24 16:30:50 -0700

    wip: Improve error handling + begin implementing paginated get route for excerpts

--- backend/src/api/routes.rs
+++ backend/src/api/routes.rs
@@ -1,8 +1,11 @@
 use crate::{
+    api::objects::responses::ExcerptWithImages,
     db::models::{Excerpt, Image},
     DbPool,
 };
+use futures::TryStreamExt;
 use poem::{
+    error::{InternalServerError, NotFoundError},
     web::{Data, Path},
     Result,
 };
@@ -11,8 +14,6 @@ use poem_openapi::{
     OpenApi,
 };
 use sqlx::Sqlite;
-
-use super::objects::responses::ExcerptWithImages;
 
 pub struct Api;
 
@@ -29,18 +30,39 @@ impl Api {
     pub async fn get_excerpt(&self, pool: Data<&DbPool>, Path(id): Path<i64>) -> Result<Json<ExcerptWithImages>> {
         let excerpt = sqlx::query_as::<Sqlite, Excerpt>("SELECT * FROM excerpt WHERE id = ?")
             .bind(id)
-            .fetch_one(pool.0)
+            .fetch_optional(pool.0)
             .await
-            .unwrap();
+            .map_err(InternalServerError)?
+            .ok_or(NotFoundError)?;
 
         let images = sqlx::query_as::<Sqlite, Image>("SELECT * FROM image WHERE post_id = ?")
             .bind(id)
             .fetch_all(pool.0)
             .await
-            .unwrap();
+            .map_err(InternalServerError)?;
 
         Ok(Json(ExcerptWithImages::new(excerpt, images.to_vec())))
     }
 
-    // TODO: Create, modify, delete + paginated get
+    // TOOD: Pagination
+    #[oai(path = "/excerpts", method = "get")]
+    pub async fn get_excerpts(&self, pool: Data<&DbPool>) -> Result<Json<Vec<ExcerptWithImages>>> {
+        let mut excerpts = sqlx::query_as::<Sqlite, Excerpt>("SELECT * FROM excerpt").fetch(pool.0);
+
+        let mut excerpt_vec = vec![];
+
+        while let Some(excerpt) = excerpts.try_next().await.map_err(InternalServerError)? {
+            let images = sqlx::query_as::<Sqlite, Image>("SELECT * FROM image WHERE post_id = ?")
+                .bind(excerpt.id)
+                .fetch_all(pool.0)
+                .await
+                .map_err(InternalServerError)?;
+
+            excerpt_vec.push(ExcerptWithImages::new(excerpt, images))
+        }
+
+        Ok(Json(excerpt_vec))
+    }
+
+    // TODO: Create, modify, delete 
 }
